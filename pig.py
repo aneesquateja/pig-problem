d2d7cf6d6bf02301bb2e97f2944322f23ba4f526
Anees Quateja <aneesq87@gmail.com>
2024-11-18 18:11:29 -0800

    here is the solution by me

--- pig.py
+++ pig.py
@@ -1,7 +1,23 @@
 def pig_latin(sentence):
     # Write your solution here!
-    pass
+  #look at the first element, if its a,e,i,o,u return the sentence itself 
+    #if not grab the first element and move it at the end 
+    # also concatenate with "ay"
+    vowels = ['a','e','i','o','u']
+    words = sentence.split()       #words = "a", "w", "e", "s", "o", "m, "e"
+    new_list = []
 
+    for letters in words:
+      if letters[0] in vowels:
+        new_list.append(letters)
+        # print(new_list)
+      else:
+          new_sentence = letters[1:] + letters[0] + "ay"
+          new_list.append(new_sentence)
+          # new_list = letters[1:] + letters[0] + "ay"
+    
+    return " ".join(new_list)
+     
 # Test cases
 assert pig_latin("something") == "omethingsay"
 assert pig_latin("awesome") == "awesome"
